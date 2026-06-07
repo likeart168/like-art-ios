f3f38ab28727cdeef54efc4b0182fb0df363c772
Monkey-ArtOps <tan851231@163.com>
2026-06-07 21:38:45 +0800

    Native iOS: WKWebView + H5 www, swiftc compile workflow

--- ios/LikeArt/AppDelegate.swift
+++ ios/LikeArt/AppDelegate.swift
@@ -1,4 +1,5 @@
 import UIKit
+import WebKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -6,12 +7,20 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
+        
+        let config = WKWebViewConfiguration()
+        config.preferences.setValue(true, forKey: "allowFileAccessFromFileURLs")
+        config.setValue(true, forKey: "allowUniversalAccessFromFileURLs")
+        
+        let webView = WKWebView(frame: UIScreen.main.bounds, configuration: config)
+        
+        if let wwwPath = Bundle.main.path(forResource: "index", ofType: "html", inDirectory: "www") {
+            let wwwURL = URL(fileURLWithPath: wwwPath)
+            webView.loadFileURL(wwwURL, allowingReadAccessTo: wwwURL.deletingLastPathComponent())
+        }
+        
         let vc = UIViewController()
-        vc.view.backgroundColor = .white
-        let label = UILabel(frame: CGRect(x: 50, y: 200, width: 300, height: 50))
-        label.text = "Like Art"
-        label.textAlignment = .center
-        vc.view.addSubview(label)
+        vc.view = webView
         window?.rootViewController = vc
         window?.makeKeyAndVisible()
         return true
