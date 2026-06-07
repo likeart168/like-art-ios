b2e4a8075de13505ea50eea41165aeff7d687424
Monkey-ArtOps <tan851231@163.com>
2026-06-07 20:59:27 +0800

    test: minimal UIKit-only app to verify build infra

--- ios/LikeArt/AppDelegate.swift
+++ ios/LikeArt/AppDelegate.swift
@@ -6,7 +6,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
-        window?.rootViewController = ViewController()
+        let vc = UIViewController()
+        vc.view.backgroundColor = .white
+        let label = UILabel(frame: CGRect(x: 50, y: 200, width: 300, height: 50))
+        label.text = "Like Art"
+        label.textAlignment = .center
+        vc.view.addSubview(label)
+        window?.rootViewController = vc
         window?.makeKeyAndVisible()
         return true
     }
